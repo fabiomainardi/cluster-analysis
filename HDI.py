8babe22d6f9fc04d197394791b372f585bd704ff
Fabio Mainardi <fabiomainardi@users.noreply.github.com>
2015-10-08 16:32:21 +0200

    Update HDI.py

--- HDI.py
+++ HDI.py
@@ -1,4 +1,3 @@
-
 # coding: utf-8
 from __future__ import print_function
 from sklearn.metrics import silhouette_samples, silhouette_score
@@ -6,24 +5,22 @@ import numpy as np
 import pandas as pd
 from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
-
-
+import matplotlib.cm as cm
+print(__doc__)
 
 data =  pd.read_csv("data.csv", delimiter = ',')
 
 hdi=data[["HDI","Rank"]].values
 range_n_clusters = [3, 4, 5, 6]
+X = hdi
+fignum = 1
+silhouette_table={}
 
 estimators={}
 for n in range_n_clusters:
     estimators['k_means_'+str(n)]= KMeans(n_clusters=n)
-     
-              
-X = hdi
-fignum = 1
-
-silhouette_table={}
-
+                  
+#plot the clusters
 for name, est in estimators.items():
 
     fig = plt.figure(fignum, figsize=(10, 7.5))
@@ -32,13 +29,13 @@ for name, est in estimators.items():
     est.fit(X)
 
     labels = est.labels_
+    
     cluster_labels = est.fit_predict(X)
     silhouette_avg = silhouette_score(X, cluster_labels)
     silhouette_table[name]=silhouette_avg
 
     sample_silhouette_values = silhouette_samples(X, cluster_labels)
     
-        
     plt.scatter(X[:,0], X[:,1], c=labels.astype(np.float))
     centers = est.cluster_centers_
     plt.scatter(centers[:,0], centers[:,1],marker='o', c="white", alpha=1, s=200)
@@ -53,7 +50,42 @@ for name, est in estimators.items():
     plt.ylabel('Rank')
     plt.xlabel('HDI')
     plt.savefig('hdi'+str(fignum-1))
-
+    
 
 t=pd.DataFrame(data=silhouette_table.items(),columns=['Date', 'DateValue'])
 print(t)
+
+#plot the silhouettes
+for n_clusters in range_n_clusters:
+  
+    fig = plt.figure(fignum, figsize=(10, 7.5))
+
+    plt.xlim([-0.1, 1])
+
+    y_lower = 10
+    for i in range(n_clusters):
+
+        ith_cluster_silhouette_values = sample_silhouette_values[cluster_labels == i]
+
+        ith_cluster_silhouette_values.sort()
+
+        size_cluster_i = ith_cluster_silhouette_values.shape[0]
+        y_upper = y_lower + size_cluster_i
+
+        color = cm.spectral(float(i) / n_clusters)
+        plt.fill_betweenx(np.arange(y_lower, y_upper),
+                          0, ith_cluster_silhouette_values,
+                          facecolor=color, edgecolor=color, alpha=0.7)
+
+        plt.text(-0.05, y_lower + 0.5 * size_cluster_i, str(i))
+
+        y_lower = y_upper + 10  
+
+
+    plt.xlabel("Silhouette coefficient values")
+    plt.ylabel("Cluster label")
+
+    plt.axvline(x=silhouette_avg, color="red", linestyle="--")
+
+    plt.savefig('Silhouette '+str(n_clusters-1))
+    plt.show()
